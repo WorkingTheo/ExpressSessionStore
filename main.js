4d7279df2527042c56d5093fd860738e1e6569dd
Fedor Selenskiy <47851444+fs1g17@users.noreply.github.com>
2023-02-08 13:33:14 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import { Low } from 'lowdb'
+import { JSONFile } from 'lowdb/node'
+
+const app = express()
+app.use(express.json())
+
+const adapter = new JSONFile('db.json')
+const db = new Low(adapter)
+await db.read()
+db.data ||= { posts: [
+  {"id":1, "value": "hello world!"}
+]}
+
+const { posts } = db.data
+
+app.get('/posts/:id', async (req, res) => {
+  const postId = req.params.id*1;
+  const post = posts.find(p => p.id === postId);
+  res.send(post)
+})
+
+app.post('/posts', async (req, res, next) => {
+  const post = { "id": req.body.id*1, "value": req.body.value};
+  console.log(post);
+
+  posts.push(post);
+
+  await db.write();
+  res.send(post);
+})
+
+app.listen(3000, () => {
+  console.log('listening on port 3000')
+})
